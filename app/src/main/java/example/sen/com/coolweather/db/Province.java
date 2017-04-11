5227ae1193484c768e93f7f30609bc8d804fbc36
wangsen <18713177522@163.com>
2017-04-11 16:45:45 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/example/sen/com/coolweather/db/Province.java
@@ -0,0 +1,41 @@
+package example.sen.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ *
+ * 省份
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    //省名
+    private String provinceName;
+    //省得代号
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
